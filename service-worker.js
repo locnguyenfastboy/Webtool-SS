1ab480ec10aa5f8fa0fc3317345b83dc6a778236
locnguyenfastboy <loc_nguyen@fastboy.net>
2024-11-08 00:46:10 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,36 +11,38 @@ const urlsToCache = [
     '/icons/icon-512x512.png'
 ];
 
-// Install event - cache files
+// Install event
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(urlsToCache);
         })
     );
+    self.skipWaiting();  // Activate this service worker immediately
 });
 
-// Fetch event - serve files from cache if available
+// Fetch event
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
+            // Return the cached response if available, otherwise fetch from network
             return response || fetch(event.request);
         })
     );
 });
 
-// Activate event - clean up old caches
+// Activate event
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
+                    if (cacheName !== CACHE_NAME) {
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
     );
+    self.clients.claim();  // Take control of all pages
 });
